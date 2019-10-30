f8e802d5775ad22bb55fca3386692a276cfdde94
JosePaulo95 <fernandes.josepaulo95@gmail.com>
2019-10-29 21:46:51 -0300

    lendo obj questao e estilo respostas certas/erradas

--- App.js
+++ App.js
@@ -9,61 +9,56 @@ import {
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { sucesso: true }
+    this.state = {
+      questao: {
+        titulo : "Quanto eh 8% de 30?",
+        alternativas: ["2", "4", "7", "10", "14"],
+        index_correta: 2
+      },
+      acertou: false,
+      index_marcada: null
+    }
   }
 
-  onPress = () => {
+  marcouAlternativa = (index) => {
     this.setState({
-      sucesso: Math.floor(Math.random()*10)>5
+      acertou: index == this.state.questao.index_correta,
+      index_marcada: index
     })
+
+
   }
 
  render() {
    return (
     <View style={styles.outside}>
       <View style={styles.container}>
-        <Text  style={styles.title}>
-          Quanto eh 1+1?
-        </Text>
-
-        <TouchableOpacity style={styles.option}>
-            <Text>
-              a. saksas aaksal 
-            </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.option}>
-            <Text>
-              a. saksas aaksal 
-            </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.option}>
-            <Text>
-              a. saksas aaksal 
-            </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.option}>
-            <Text>
-              a. saksas aaksal 
-            </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.option}>
-            <Text>
-              a. saksas aaksal 
-            </Text>
-        </TouchableOpacity>
-
         <View style={styles.footer}>
+          <Text>
+            &#9719;
+          </Text>
           <View style={styles.footerBar}>
 
           </View>
-          <Text style={styles.footerText}>
-            16 questoes respondidas
-          </Text>
         </View>
+        <Text  style={styles.title}>
+          {this.state.questao.titulo}
+        </Text>
+
+        {
+          this.state.questao.alternativas.map((alternativa, index) => 
+            <TouchableOpacity
+              onPress={()=>this.marcouAlternativa(index)}
+              key={index}
+              style={this.state.index_marcada == index ? this.state.acertou?styles.correctOption:styles.wrongOption:styles.option}
+              //disabled={this.state.index_marcada != null}
+            >
+                <Text style={this.state.index_marcada == index ? this.state.acertou?styles.correctOptionText:styles.wrongOptionText:styles.optionText}>
+                  {alternativa} {this.state.index_marcada == index ? this.state.acertou?"✓":"✘":""}
+                </Text>
+            </TouchableOpacity>
+          )
+        }
       </View>
     </View>
     )
@@ -73,7 +68,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   outside:{
     flex: 1,
-    backgroundColor: "#008983",
+    backgroundColor: "rgb(207, 216, 220)",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -88,20 +83,17 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0, 0, 0, 0.05)",
     padding: 15,
     justifyContent: "center",
-    flexDirection: 'column',
+    flexDirection: 'row',
     borderTopColor: "rgba(0, 0, 0, 0.1)",
     borderTopWidth: 2,
     alignItems: "center"
   },
   footerBar:{
-    width: 170,
-    height: 20,
-    margin: 8,
+    marginLeft: 5,
+    width: 240,
+    height: 12,
     borderRadius: 20,
     backgroundColor: "red",
-  },
-  footerText:{
-    
   },
   title:{ 
     textAlign: "center",
@@ -118,6 +110,36 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginBottom: 12,
     backgroundColor: "rgba(0, 0, 0, 0.08)",
+  },
+  correctOption :{
+    borderRadius: 290486,
+    padding: 10,
+    paddingLeft: 15,
+    margin: 3,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 12,
+    backgroundColor: "rgb(0, 204, 153)",
+  },
+  wrongOption :{
+    borderRadius: 290486,
+    padding: 10,
+    paddingLeft: 15,
+    margin: 3,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 12,
+    backgroundColor: "rgb(230,110,94)",
+  },
+  optionText:{
     color: "rgba(0, 0, 0, 0.85)"
+  },
+  correctOptionText:{
+    color: "rgba(250, 250, 250, 0.85)",
+    fontWeight: "bold"
+  },
+  wrongOptionText:{
+    color: "rgba(250, 250, 250, 0.85)",
+    fontWeight: "bold"
   }
 })
